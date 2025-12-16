361f3f5b0eea6e9843b1f752f6403554a9733d63
hakim <samouh591@gmail.com>
2025-12-16 13:36:52 +0100

    Modif shared config

--- vars/javaPipeline.groovy
+++ vars/javaPipeline.groovy
@@ -1,35 +1,47 @@
 def call(Map config = [:]) {
+
+    def buildType = config.buildType ?: 'maven'
+    def buildCmd  = config.buildCmd  ?: ''
+
     pipeline {
         agent any
         stages {
             stage('Build') {
                 steps {
-                    echo "Building project..."
-                    sh 'echo mvn clean package'
+                    script {
+                        if (buildType == 'maven') {
+                            sh buildCmd ?: 'mvn clean package'
+                        }
+                        else if (buildType == 'node') {
+                            sh buildCmd ?: 'npm install && npm run build'
+                        }
+                        else if (buildType == 'python') {
+                            sh buildCmd ?: 'pip install -r requirements.txt'
+                        }
+                        else {
+                            error "Build type not supported: ${buildType}"
+                        }
+                    }
                 }
             }
+
             stage('Test') {
+                when {
+                    expression { config.runTests != false }
+                }
                 steps {
-                    echo "Running tests..."
-                    sh 'echo mvn test'
+                    echo "Running tests"
                 }
             }
+
             stage('Deploy') {
                 when {
                     expression { config.env == 'prod' }
                 }
                 steps {
                     echo "Deploying to ${config.env}"
-                    sh "echo ./deploy.sh ${config.env}"
                 }
-            }
-        }
-        post {
-            always {
-                echo "Cleaning workspace"
-                cleanWs()
             }
         }
     }
 }
-

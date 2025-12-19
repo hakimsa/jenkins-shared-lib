9a1c3a08b9717b45a341798751eeb30a259c977c
hakim <samouh591@gmail.com>
2025-12-20 00:11:34 +0100

    add maven image

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,13 +1,13 @@
 def call(Map config = [:]) {
 
-    def buildType = config.buildType ?: detectBuildType()
+    def buildType = config.buildType ?: detectBuildType()  // node, maven, python
     def buildCmd  = config.buildCmd
 
     pipeline {
-        agent any
-
+        agent {
+        docker { image 'maven:3.9.0' }}
+      
         stages {
-
             stage('Checkout') {
                 steps {
                     checkout scm
@@ -20,16 +20,18 @@ def call(Map config = [:]) {
                 }
             }
 
-            stage('Build') {
+            stage('Install / Build') {
                 steps {
                     script {
-                        if (buildType == 'maven') {
+                        if (buildType == 'node') {
+                            sh buildCmd ?: 'npm install'
+                            sh 'npm run build:prod || echo "No build step defined"'
+                        } else if (buildType == 'maven') {
                             sh buildCmd ?: 'mvn clean package'
-                        } else if (buildType == 'node') {
-                            sh 'npm install'
-                            sh 'npm run build:prod || echo "No build step"'
+                        } else if (buildType == 'python') {
+                            sh buildCmd ?: 'pip install -r requirements.txt'
                         } else {
-                            error "Unsupported build type"
+                            error "Unsupported build type: ${buildType}"
                         }
                     }
                 }
@@ -38,44 +40,46 @@ def call(Map config = [:]) {
             stage('Test') {
                 steps {
                     script {
-                        if (buildType == 'maven') {
+                        if (buildType == 'node') {
+                            sh 'npm test || echo "No tests found"'
+                        } else if (buildType == 'maven') {
                             sh 'mvn test'
-                        } else if (buildType == 'node') {
-                            sh 'npm test || echo "No tests"'
+                        } else if (buildType == 'python') {
+                            sh 'pytest || echo "No tests found"'
                         }
                     }
                 }
             }
+         steps {
+        withCredentials([
+            usernamePassword(
+                credentialsId: 'dockerhub-creds',
+                usernameVariable: 'DOCKER_USER',
+                passwordVariable: 'DOCKER_PASS'
+            )
+        ]) {
+            script {
+                def imageName = "hakimsamouh/my-app:${env.BUILD_NUMBER}"
 
-            stage('Docker Build & Push') {
-                steps {
-                    withCredentials([
-                        usernamePassword(
-                            credentialsId: 'dockerhub-creds',
-                            usernameVariable: 'DOCKER_USER',
-                            passwordVariable: 'DOCKER_PASS'
-                        )
-                    ]) {
-                        script {
-                            sh '''
-                              docker build -t app-mgt:${BUILD_NUMBER} .
-                              docker tag app-mgt:${BUILD_NUMBER} $DOCKER_USER/app-mgt:${BUILD_NUMBER}
-                              echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-                              docker push $DOCKER_USER/app-mgt:${BUILD_NUMBER}
-                            '''
-                            env.DOCKER_IMAGE = "$DOCKER_USER/app-mgt:${BUILD_NUMBER}"
-                        }
-                    }
-                }
+                sh '''
+                  docker build -t app-mgt:${BUILD_NUMBER} .
+                  docker tag app-mgt:${BUILD_NUMBER} ${DOCKER_USER}/app-mgt:${BUILD_NUMBER}
+                  echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
+                  docker push ${DOCKER_USER}/app-mgt:${BUILD_NUMBER}
+                '''
+
+                env.DOCKER_IMAGE = "${DOCKER_USER}/app-mgt:${env.BUILD_NUMBER}"
             }
+        }
+    }
+
 
             stage('Deploy') {
                 steps {
                     script {
-                        sh '''
-                          docker rm -f app-mgt || true
-                          docker run -d --name app-mgt -p 3000:3000 $DOCKER_IMAGE
-                        '''
+                        def containerName = "app-mgt-${env.BUILD_NUMBER}"
+                        sh "docker rm -f ${containerName} || true"
+                        sh "docker run -d --name ${containerName} -p 3000:3000 ${env.DOCKER_IMAGE}"
                     }
                 }
             }
@@ -83,8 +87,9 @@ def call(Map config = [:]) {
 
         post {
             always {
-                echo "Pipeline finished"
+                echo "Pipeline finished!"
             }
         }
     }
+
 }

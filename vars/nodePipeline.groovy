19e4d47e2003f36f2119e6eeb16919c8b465afe4
hakimsa <samouh591@gmail.com>
2026-02-05 17:58:55 +0100

    test lib nodejs

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -5,16 +5,65 @@ def call(Map config = [:]) {
         tools {
             nodejs 'node-18'
         }
+ stages {
 
-        stages {
             stage('Install') {
                 steps {
-                    sh 'node -v'
-                    sh 'npm -v'
-                    sh 'npm install'
-                    sh ' echo listar dir'
-                    sh 'ls -ltr'
+                    sh '''
+                        node -v
+                        npm -v
+                        npm ci
+                    '''
                 }
+            }
+
+            stage('Test') {
+                when {
+                    expression { fileExists('package.json') }
+                }
+                steps {
+                    sh 'npm test'
+                }
+            }
+
+            stage('Build') {
+                when {
+                    anyOf {
+                        fileExists('dist')
+                        fileExists('build')
+                        expression { sh(script: "jq -e '.scripts.build' package.json > /dev/null", returnStatus: true) == 0 }
+                    }
+                }
+                steps {
+                    sh 'npm run build || echo "No build step"'
+                }
+            }
+
+            stage('Package') {
+                steps {
+                    sh '''
+                        mkdir -p package
+
+                        cp package.json package/
+                        [ -f package-lock.json ] && cp package-lock.json package/
+
+                        [ -d dist ] && cp -r dist package/
+                        [ -d build ] && cp -r build package/
+                        [ -f ecosystem.config.js ] && cp ecosystem.config.js package/
+                        [ -f .env.example ] && cp .env.example package/
+
+                        cp -r node_modules package/
+
+                        tar -czf ${APP_NAME}.tar.gz package
+                    '''
+                }
+            }
+
+        }
+
+        post {
+            success {
+                archiveArtifacts artifacts: '*.tar.gz', fingerprint: true
             }
         }
     }

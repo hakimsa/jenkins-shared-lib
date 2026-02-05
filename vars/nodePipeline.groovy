9ddbb950f047424393cf5b5a372569b008f482c5
hakimsa <samouh591@gmail.com>
2026-02-05 16:05:31 +0100

    test lib nodejs

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -1,9 +1,16 @@
 def call(Map config = [:]) {
     pipeline {
         agent any
+
+        tools {
+            nodejs 'node25'
+        }
+
         stages {
             stage('Install') {
                 steps {
+                    sh 'node -v'
+                    sh 'npm -v'
                     sh 'npm install'
                 }
             }

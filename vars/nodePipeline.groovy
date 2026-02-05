81fb01e50368851f9e25a881458620d85c8b1d46
hakimsa <samouh591@gmail.com>
2026-02-05 15:54:04 +0100

    test lib nodejs

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -1,36 +1,10 @@
 def call(Map config = [:]) {
     pipeline {
         agent any
-
-        environment {
-            NODE_ENV = config.nodeEnv ?: 'production'
-        }
-
         stages {
-            stage('Checkout') {
-                steps {
-                    checkout scm
-                }
-            }
-
-            stage('Install dependencies') {
+            stage('Install') {
                 steps {
                     sh 'npm install'
-                }
-            }
-
-            stage('Test') {
-                steps {
-                    sh 'npm test'
-                }
-            }
-
-            stage('Build') {
-                when {
-                    expression { fileExists('package.json') }
-                }
-                steps {
-                    sh 'npm run build'
                 }
             }
         }
